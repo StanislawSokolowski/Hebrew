e31289e78fd0d34838df95302680036e4d017cdb
StanislawSokolowski <stanislaw.sokolowski@tempora.pl>
2025-02-10 19:43:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,43 @@
-const CACHE_NAME = 'hebrew-cache-v1';
+// A simple service worker for offline caching
+
+const CACHE_NAME = "hebrew-flashcards-v1";
 const FILES_TO_CACHE = [
-  'index.html',
-  'styles.css',
-  'app.js',
-  'manifest.json',
-  // Include icons and any other assets you want to cache:
-  'icons/icon-192.png',
-  'icons/icon-512.png'
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
+self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
+      console.log("Pre-caching offline resources");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener("activate", (evt) => {
   evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  // Serve cached content when offline.
+self.addEventListener("fetch", (evt) => {
   evt.respondWith(
     caches.match(evt.request).then((response) => {
       return response || fetch(evt.request);
